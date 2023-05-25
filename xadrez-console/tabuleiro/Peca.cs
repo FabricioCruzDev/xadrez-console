24e81d28da00b884214657113c5833b350a698cf
Fabricio <fabriciodev@outlook.com>
2023-05-25 11:09:59 -0300

    Começando a colocar peças no tabuleiro

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -8,9 +8,9 @@ namespace tabuleiro
         public int QteMovimentos { get; set; }
         public Tabuleiro Tab { get; set; }
 
-        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        public Peca(Tabuleiro tab, Cor cor)
         {
-            Posicao = posicao;
+            Posicao = null;
             Cor = cor;
             QteMovimentos = 0;
             Tab = tab;
