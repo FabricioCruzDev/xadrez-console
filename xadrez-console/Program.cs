19f613b89e0bd3cd9428711d752051b2cf81492b
Fabricio <fabriciodev@outlook.com>
2023-05-27 10:30:06 -0300

    Cirando a classe PosiçãoXadrez

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -9,7 +9,7 @@ namespace xadrez_console
         static void Main(string[] args)
         {
 
-            try
+            /*try
             {
                 Tabuleiro tab = new Tabuleiro(8, 8);
 
@@ -24,6 +24,13 @@ namespace xadrez_console
             {
                 Console.WriteLine(e.Message);
             }
+            */
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+
+            Console.WriteLine(pos);
+
+            Console.WriteLine(pos.ToPosicao());
+
 
             Console.ReadLine();
         }
